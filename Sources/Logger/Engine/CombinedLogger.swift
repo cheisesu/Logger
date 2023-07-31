cadd72387384b170bc07d2307d92a0683c6b3f98
Dmitrii Shelonin <cheisesu@gmail.com>
2023-07-31 17:38:54 +0400

    - added combined logger

---
+++ Sources/Logger/Engine/CombinedLogger.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+/// Logger that combines other engines and acts as proxy
+public class CombinedLogger: LoggerEngine {
+    private let engines: [LoggerEngine]
+
+    /// Constructs with other engines
+    /// - Parameter engines: Array of engine instances
+    public init(engines: [LoggerEngine]) {
+        self.engines = engines
+    }
+
+    /// Constructs with other engines
+    /// - Parameter engines: Engine instances
+    public init(_ engines: LoggerEngine...) {
+        self.engines = engines
+    }
+
+    public func write(_ message: String, of category: LoggerCategory? = nil, as logType: LogType = .default,
+                      _ file: String = #fileID, _ line: Int = #line) {
+        engines.forEach { engine in
+            engine.write(message, of: category, as: logType, file, line)
+        }
+    }
+
+    public func flush() {
+        engines.forEach { engine in
+            engine.flush()
+        }
+    }
+}
