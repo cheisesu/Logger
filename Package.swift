7c1f8e19345c54edf638af31e9d54ea8a868b702
Dmitrii Shelonin <cheisesu@gmail.com>
2023-08-06 16:07:29 +0400

    - just a commit

--- Package.swift
+++ Package.swift
@@ -25,10 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Logger",
-            dependencies: [],
-            swiftSettings: [
-                .define("BUILD_LIBRARY_FOR_DISTRIBUTION")
-            ]
+            dependencies: []
         ),
         .testTarget(
             name: "LoggerTests",
