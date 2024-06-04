1c2558f7b1ab98a66727f6ac332d83262c3e5f38
Dmitrii Shelonin <cheisesu@gmail.com>
2024-06-04 19:44:15 +0200

    - added tests for logger engine extension

--- Sources/Logger/Engine/LoggerEngine.swift
+++ Sources/Logger/Engine/LoggerEngine.swift
@@ -30,12 +30,12 @@ extension LoggerEngine {
     /// - Parameters:
     ///   - items: Zero or more items to write.
     ///   - category: Category of the message to indicate it in a system. The default value is nil.
-    ///   - logType: Type of the message.
+    ///   - logType: Type of the message. The default value is <doc:LogType/default>.
     ///   - separator: A string to print between each item. The default is a single space (`" "`).
     ///   - terminator: The string to print after all items have been printed. The default is a newline (`"\n"`).
     ///   - file: File, where the method is called. The default value is `#fileID`.
     ///   - line: Line in the file. The default value is a number of line of the call (`#line`).
-    public func write(_ items: Any..., category: LoggerCategory? = nil, logType: LogType,
+    public func write(_ items: Any..., category: LoggerCategory? = nil, logType: LogType = .default,
                       separator: String = " ", terminator: String = "\n",
                       file: String = #fileID, line: Int = #line)
     {
--- Tests/LoggerTests/LoggerEngine_Write_tests.swift
+++ Tests/LoggerTests/LoggerEngine_Write_tests.swift
@@ -5,6 +5,20 @@ final class LoggerEngine_Write_tests: XCTestCase {
     override func setUp() {
         continueAfterFailure = false
     }
+
+    func test_ExtendedWriteItems_CallesSourceWriteWithCorrectArguments() throws {
+        let engine = _MockedLoggerEngine()
+        let file = #fileID
+
+        let line = #line; engine.write("source", "message", category: "category")
+
+        XCTAssertTrue(engine.writeCalled)
+        XCTAssertEqual(engine.writeItems?.map { String(describing: $0) }, ["source", "message"])
+        XCTAssertEqual(engine.writeCategory?.rawLoggerCategory, "category")
+        XCTAssertEqual(engine.writeLogType, .default)
+        XCTAssertEqual(engine.writeFile, file)
+        XCTAssertEqual(engine.writeLine, line)
+    }
 }
 
 // MARK: - DEPRECATIONS
