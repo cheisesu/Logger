b17b46e7adffa0c3b9129959f36fce29fd6b0162
Dmitrii Shelonin <cheisesu@gmail.com>
2025-03-29 13:08:02 +0100

    [async] added async FileLoggerStream;

---
+++ Sources/Logger/LoggerStream/FileLoggerStream+Async.swift
@@ -0,0 +1,99 @@
+import Foundation
+
+extension FileLoggerStream {
+    public actor Async: LoggerStream {
+        private let sourceURL: URL
+        private var fileHandle: FileHandle
+        private let fileManager: FileManager
+        private let fileLimits: FileLimitsPolitics
+        private let fileTransferPolicy: FileTransferPolicy?
+        private var currentSize: Measurement<UnitInformationStorage>
+        private let encoding: String.Encoding
+        private var transformers: [FileLoggerStreamTransformable]
+
+        public init(_ sourceURL: URL, encoding: String.Encoding = .utf8, fileManager: FileManager = .default,
+                    fileLimits: FileLimitsPolitics = 0, fileTransferPolicy: FileTransferPolicy? = nil) throws {
+            self.sourceURL = sourceURL
+            self.fileManager = fileManager
+            self.fileLimits = fileLimits
+            self.fileTransferPolicy = fileTransferPolicy
+            self.encoding = encoding
+            transformers = []
+
+            if !fileManager.fileExists(atPath: sourceURL.path) {
+                try Data().write(to: sourceURL)
+            }
+            fileHandle = try FileHandle(forUpdating: sourceURL)
+            if #available(macOS 10.15.4, iOS 13.4, tvOS 13.4, *) {
+                let data = try fileHandle.readToEnd() ?? Data()
+                currentSize = Measurement(value: Double(data.count), unit: .bytes)
+            } else {
+                let data = fileHandle.readDataToEndOfFile()
+                currentSize = Measurement(value: Double(data.count), unit: .bytes)
+            }
+        }
+
+        deinit {
+            do {
+                try fileHandle.close()
+            } catch {
+            }
+        }
+
+        public func addTransformer(_ transformer: FileLoggerStreamTransformable) {
+            transformers.append(transformer)
+        }
+
+        public nonisolated func write(_ string: String) {
+            Task {
+                await _write(string)
+            }
+        }
+
+        private func _write(_ string: String) {
+            do {
+                guard var data = string.data(using: encoding) else { return }
+                if currentSize >= fileLimits.maxSize {
+                    changeFile()
+                }
+                data = try transformedData(data)
+                if #available(tvOS 13.4, macOS 10.15.4, iOS 13.4, *) {
+                    try fileHandle.write(contentsOf: data)
+                } else {
+                    fileHandle.write(data)
+                }
+                let size = Measurement(value: Double(data.count), unit: UnitInformationStorage.bytes)
+                handleWriteSize(size)
+            } catch {
+            }
+        }
+
+        private func handleWriteSize(_ size: Measurement<UnitInformationStorage>) {
+            currentSize = currentSize + size
+            if currentSize >= fileLimits.maxSize {
+                changeFile()
+            }
+        }
+
+        /// - warning: this method should be used under `fileHandle` lock and `currentSize `lock.
+        private func changeFile() {
+            guard let fileTransferPolicy else { return }
+            do {
+                try fileHandle.synchronize()
+                try fileHandle.close()
+                try fileTransferPolicy.perform(for: sourceURL, recreateSource: true)
+                fileHandle = try FileHandle(forUpdating: sourceURL)
+                currentSize = Measurement(value: 0, unit: .bytes)
+            } catch {
+            }
+        }
+
+        private func transformedData(_ data: Data) throws -> Data {
+            var data = data
+            for transformer in transformers {
+                data = try transformer.transform(data)
+            }
+            return data
+        }
+    }
+}
