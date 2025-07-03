05f1582bfed0ab54be1af58ff01bbbea0667bd6a
Dmitrii Shelonin <cheisesu@gmail.com>
2025-07-03 23:28:11 +0200

    [async-version] StreamedLogger async conformance;

--- Sources/LoggerKit/Engine/StreamedLogger.swift
+++ Sources/LoggerKit/Engine/StreamedLogger.swift
@@ -1,28 +1,49 @@
 import Foundation
 
-public final class StreamedLogger: LoggerEngine, @unchecked Sendable {
+public final class StreamedLogger: @unchecked Sendable {
     private let defaultCategory: LoggerCategory
     private let messageConstructor: LoggerMessageConstructor
-    private let streamLock: NSLock
+    private let accessQueue: DispatchQueue
     private var stream: LoggerStream
 
-    public init(defaultCategory: LoggerCategory = "default",
-                messageConstructor: LoggerMessageConstructor = .default,
+    public init(defaultCategory: LoggerCategory = "default", messageConstructor: LoggerMessageConstructor = .default,
                 stream: LoggerStream) {
+        accessQueue = DispatchQueue(label: "com.loggerkit.logger.streamed")
         self.defaultCategory = defaultCategory
         self.messageConstructor = messageConstructor
-        streamLock = NSLock()
         self.stream = stream
     }
+}
 
+// MARK: - LOGGER ENGINE CONFORMANCE
+
+extension StreamedLogger: LoggerEngine {
     public func write(_ items: [Any], category: (any LoggerCategory)?, logType: LogType,
                       separator: String, terminator: String, file: String, line: Int)
     {
         let message = messageConstructor.makeMessage(from: items, category: category ?? defaultCategory, logType: logType,
                                                      separator: separator, terminator: terminator, file: file, line: line)
         guard let message else { return }
-        streamLock.lock()
-        defer { streamLock.unlock() }
-        stream.write(message)
+        accessQueue.sync {
+            stream.write(message)
+        }
+    }
+}
+
+// MARK: - ASYNC LOGGER ENGINE CONFORMANCE
+
+extension StreamedLogger: AsyncLoggerEngine {
+    public func writeAsync(_ items: [any Sendable], category: (any LoggerCategory)?, logType: LogType,
+                      separator: String, terminator: String, file: String, line: Int) async
+    {
+        let message = messageConstructor.makeMessage(from: items, category: category ?? defaultCategory, logType: logType,
+                                                     separator: separator, terminator: terminator, file: file, line: line)
+        guard let message else { return }
+        return await withCheckedContinuation { continuation in
+            self.accessQueue.async { [weak self] in
+                defer { continuation.resume() }
+                self?.stream.write(message)
+            }
+        }
     }
 }
